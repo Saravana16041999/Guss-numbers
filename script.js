6d5c7cb30714a407835db524fd7e4a9244db12e4
Saravana16041999 <saravanakumar16041999@gmail.com>
2022-11-08 19:02:14 +0530

    ui-completed

---
+++ script.js
@@ -0,0 +1,13 @@
+`use strict`
+
+// score bords
+const highScoretopEl = document.getElementById(`highscore_one`);
+const highscoreEl = document.getElementById(`highscore`)
+const ScoreEl = document.getElementById(`score`)
+
+//getting from users
+const inputEl = document.getElementById(`userNumber`);
+const submitEl = document.getElementById('check')
+
+//restart the hole game
+const resetEl = document.getElementById('reset')
