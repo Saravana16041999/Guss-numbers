8f2aca00226d06161d520c264cbe633f6c1440f9
Saravana16041999 <saravanakumar16041999@gmail.com>
2022-11-08 19:17:01 +0530

    dfgh

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ const submitEl = document.getElementById('check')
 
 //restart the hole game
 const resetEl = document.getElementById('reset')
+// msg
+const msg = document.getElementById(`msg`)
+
+
+
+submitEl.addEventListener('click',()=>{
+    let number = Math.ceil(Math.random() * 20)
+    
+    if(inputEl ===''){
+        msg.innerText = `please give valide answer`
+    }
+})
