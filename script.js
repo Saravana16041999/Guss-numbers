9c90d1fa2e511cb1d8cec99731cc904eb68b6b22
Saravana16041999 <saravanakumar16041999@gmail.com>
2022-11-09 17:12:57 +0530

    fully updated

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const ScoreEl = document.getElementById(`score`)
 //getting from users
 const inputEl = document.getElementById(`userNumber`);
 const submitEl = document.getElementById('check')
+const chanceEl = document.getElementById('chance')
 
 //restart the hole game
 const resetEl = document.getElementById('reset')
@@ -15,14 +16,16 @@ const resetEl = document.getElementById('reset')
 const msg = document.getElementById(`msg`)
 
 let number = Math.ceil(Math.random() * 20)
-let highscore = 0
-let score = 0
+let highscore = 0;
+let score = 0;
+chance = 5;
 
 //functions:
 function init(){
     highScoretopEl.innerText = highscore;
     highscoreEl.innerText = highscore;
     ScoreEl.innerText = score;
+    chanceEl.innerText = chance
 }
 
 massage = {
@@ -35,39 +38,56 @@ massage = {
 
 submitEl.addEventListener('click',()=>{
     let guss = Math.round(Number(inputEl.value)); 
-
-    if(!guss || guss < 0){
-        msg.innerText = 'invalide input try again'
-    }else{
-        if(guss === number){
-            score  = score+1
-            msg.innerText = 'Nice One'
-            let liEl = document.createElement('li')
-            msg.appendChild(liEl)
-            liEl.innerText = massage.win
-            number = Math.ceil(Math.random() * 20)
-        }else if(guss > number){
-            msg.innerText = 'Nice One'
-            let liEl = document.createElement('li')
-            msg.appendChild(liEl)
-            liEl.innerText = massage.Lower
+    if(chance > 1){
+        if(!guss || guss < 0){
+            msg.innerText = 'invalide input try again'
         }else{
-            msg.innerText = 'Nice One'
+            if(guss === number){
+                score  = score+1
+                msg.innerText = 'Nice One'
+                let liEl = document.createElement('li')
+                msg.appendChild(liEl)
+                liEl.innerText = massage.win
+                number = Math.ceil(Math.random() * 20)
+                chance = 5;
+            }else if(guss > number){
+                msg.innerText = 'Nice One'
+                let liEl = document.createElement('li')
+                msg.appendChild(liEl)
+                liEl.innerText = massage.Lower
+                chance--;
+            }else{
+                msg.innerText = 'Nice One'
+                let liEl = document.createElement('li')
+                msg.appendChild(liEl)
+                liEl.innerText = massage.Higher
+                chance--
+            }  
+        }
+    }else{
+            msg.innerText = 'you Lost'
             let liEl = document.createElement('li')
             msg.appendChild(liEl)
-            liEl.innerText = massage.Higher
-        }  
+            liEl.innerText = 'click reset to try again'
+            highscore = score
+            number = Math.ceil(Math.random() * 20)
+            score = 0
     }
+    highScoretopEl.innerText = highscore + score
+    inputEl.value = null
     init()
 })
 
 resetEl.addEventListener('click',()=>{
     ScoreEl.innerText = '0'
-    highScoretopEl.innerText = '0'
+    highscoreEl.innerText = '0'
     number = Math.ceil(Math.random() * 20)
     highscore = score
+    chanceEl.innerText = chance
+    score = 0
     init()
 })
+
 
 
 
