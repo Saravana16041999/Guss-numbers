72983d5dc7131c52d0f2406755a26b1461c60a76
Saravana16041999 <saravanakumar16041999@gmail.com>
2022-11-09 16:43:34 +0530

    half Js-completed

--- script.js
+++ script.js
@@ -14,12 +14,61 @@ const resetEl = document.getElementById('reset')
 // msg
 const msg = document.getElementById(`msg`)
 
+let number = Math.ceil(Math.random() * 20)
+let highscore = 0
+let score = 0
+
+//functions:
+function init(){
+    highScoretopEl.innerText = highscore;
+    highscoreEl.innerText = highscore;
+    ScoreEl.innerText = score;
+}
+
+massage = {
+    win : '🎉 You Won!..',
+    Higher : '☝🏾 It is Higher..',
+    Lower : '👎🏾 It is Lower'
+}
+
 
 
 submitEl.addEventListener('click',()=>{
-    let number = Math.ceil(Math.random() * 20)
-    
-    if(inputEl ===''){
-        msg.innerText = `please give valide answer`
+    let guss = Math.round(Number(inputEl.value)); 
+
+    if(!guss || guss < 0){
+        msg.innerText = 'invalide input try again'
+    }else{
+        if(guss === number){
+            score  = score+1
+            msg.innerText = 'Nice One'
+            let liEl = document.createElement('li')
+            msg.appendChild(liEl)
+            liEl.innerText = massage.win
+            number = Math.ceil(Math.random() * 20)
+        }else if(guss > number){
+            msg.innerText = 'Nice One'
+            let liEl = document.createElement('li')
+            msg.appendChild(liEl)
+            liEl.innerText = massage.Lower
+        }else{
+            msg.innerText = 'Nice One'
+            let liEl = document.createElement('li')
+            msg.appendChild(liEl)
+            liEl.innerText = massage.Higher
+        }  
     }
+    init()
 })
+
+resetEl.addEventListener('click',()=>{
+    ScoreEl.innerText = '0'
+    highScoretopEl.innerText = '0'
+    number = Math.ceil(Math.random() * 20)
+    highscore = score
+    init()
+})
+
+
+
+init()
